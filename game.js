5bcc910d984900c6e07c7d4bf97bc0c0fa6f3bfe
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-23 08:10:07 +0530

    Created the basic Logic of the Game and implemented it to run in Console

---
+++ game.js
@@ -0,0 +1,94 @@
+const getUserChoice = (userInput)=> {
+   let userChoice=userInput.toLowerCase();
+    if(userChoice === 'rock'||'paper'||'scissors'){
+        console.log(`You choose ${userChoice}`);
+        return userChoice;
+    }
+    else{
+        console.log('Invalid Selection');
+    }
+};
+
+
+const getComputerChoice =  ()=> {
+  let randomNo = Math.floor(Math.random() * 3 + 1);
+  var computerChoice;
+  switch (randomNo) {
+    case 1:
+      computerChoice ="rock";
+      break;
+    case 2:
+      computerChoice= "paper";
+      break;
+    case 3:
+     computerChoice = "scissors";
+      break;
+  }
+  console.log(`computer choose ${computerChoice}`);
+  return computerChoice;
+};
+
+
+const playRound =(userChoice,computerChoice)=>{
+    
+    if (userChoice === computerChoice) {
+        return 'The game is a tie';
+      }//Both select the Same choice
+    
+      if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+          return 'You Lost!';
+        } else {
+          return "You Won!"
+        }
+      } // end userchoice is rock
+
+    
+      if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+          return 'You Lost!';
+        } else {
+          return 'You won!';
+        }
+      } // end userchoice is paper
+    
+    
+      if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+          return 'You Lost!';
+        } else {
+          return 'You Won!';
+        }
+      } //end userchoice is scissors
+    
+};
+function game(){
+    // let userInput=prompt("Enter your choice" );
+    let a= getUserChoice("rock");
+    let b= getComputerChoice();
+    let result=playRound(a,b);
+    console.log(result);
+    return result;
+}
+let flag=5;
+let i=1;
+let Pc=0;
+let human=0;
+while(i<=5){
+   let result= game();
+    if("You Won!"===result){
+        human= human+1;
+        console.log(`------Human ${human}------Computer ${Pc}-------`);
+        i++;
+    }
+    else if("You Lost!"===result){
+        Pc=Pc+1;
+        console.log(`------Human ${human}------Computer ${Pc}-------`);
+        i++;
+    }
+}
+if(human>Pc){
+    console.log(`Human ${human} Computer ${Pc}----human wins all the game`);
+}
+else
+console.log(`Computer ${Pc} Human ${human}----Computer wins all the game`);
