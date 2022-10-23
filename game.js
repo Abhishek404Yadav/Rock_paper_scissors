2deb366cb20cd814a18f7b0de76860f27e320ab5
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-10-23 08:33:38 +0530

    Cleaned the code and made it more readable

--- game.js
+++ game.js
@@ -8,26 +8,26 @@ const getUserChoice = (userInput)=> {
         console.log('Invalid Selection');
     }
 };
-
-
+// ------------------------------------------
 const getComputerChoice =  ()=> {
-  let randomNo = Math.floor(Math.random() * 3 + 1);
-  var computerChoice;
-  switch (randomNo) {
-    case 1:
-      computerChoice ="rock";
+    let randomNo = Math.floor(Math.random() * 3 + 1);
+    let computerChoice;
+    switch (randomNo) {
+        case 1:
+            computerChoice ="rock";
       break;
     case 2:
-      computerChoice= "paper";
-      break;
-    case 3:
-     computerChoice = "scissors";
-      break;
+        computerChoice= "paper";
+        break;
+        case 3:
+            computerChoice = "scissors";
+            break;
   }
   console.log(`computer choose ${computerChoice}`);
   return computerChoice;
 };
 
+// ------------------------------------------
 
 const playRound =(userChoice,computerChoice)=>{
     
@@ -62,18 +62,17 @@ const playRound =(userChoice,computerChoice)=>{
       } //end userchoice is scissors
     
 };
+
 function game(){
-    // let userInput=prompt("Enter your choice" );
-    let a= getUserChoice("rock");
-    let b= getComputerChoice();
-    let result=playRound(a,b);
+    let userInput=prompt("Enter your choice" );
+    let result=playRound(getUserChoice(userInput),getComputerChoice());
     console.log(result);
     return result;
 }
-let flag=5;
-let i=1;
+
 let Pc=0;
 let human=0;
+let i=1;
 while(i<=5){
    let result= game();
     if("You Won!"===result){
@@ -87,8 +86,5 @@ while(i<=5){
         i++;
     }
 }
-if(human>Pc){
-    console.log(`Human ${human} Computer ${Pc}----human wins all the game`);
-}
-else
-console.log(`Computer ${Pc} Human ${human}----Computer wins all the game`);
+(human>Pc) ? console.log(`Human ${human} Computer ${Pc}----human wins all the game`)
+:console.log(`Computer ${Pc} Human ${human}----Computer wins all the game`);
