6ea9a6ed38f5c298baf52e8dd27111a966c6041f
Abhishek Yadav <abhiyadav.ce@gmail.com>
2022-11-02 03:28:24 +0530

    Game is fixed and running with the core logic on console

--- game.js
+++ game.js
@@ -1,90 +1,79 @@
-const getUserChoice = (userInput)=> {
-   let userChoice=userInput.toLowerCase();
-    if(userChoice === 'rock'||'paper'||'scissors'){
-        console.log(`You choose ${userChoice}`);
-        return userChoice;
-    }
-    else{
-        console.log('Invalid Selection');
-    }
-};
-// ------------------------------------------
-const getComputerChoice =  ()=> {
-    let randomNo = Math.floor(Math.random() * 3 + 1);
-    let computerChoice;
-    switch (randomNo) {
-        case 1:
-            computerChoice ="rock";
+const userScore = 0;
+const computerScore = 0;
+// !DOM cache variables
+const roundInfo_ui = document.getElementById("roundInfo");
+const roundResult_ui = document.getElementById("roundResult");
+const userSign_ui = document.getElementById("userSign");
+const computerSign_ui = document.getElementById("computerSign");
+const userScore_ui = document.getElementById("userScore");
+const computerScore_ui = document.getElementById("computerScore");
+
+const userChoiceArr = Array.from(document.querySelectorAll("[data-choice]"));
+
+//-----This event is run when we click any button----
+for (const element of userChoiceArr) {
+  element.addEventListener("click", () => {
+    const selectedChoice = element.dataset.choice;
+    game(selectedChoice);
+  });
+}
+//!-------End here--------
+
+//------This game function call playRound function-----
+function game(userSelection) {
+  const computerChoice = getComputerChoice();
+  console.log(playRound(userSelection, computerChoice));
+}
+//!-------End here--------
+
+//------This is computer Choice function-----
+const getComputerChoice = () => {
+  let randomNo = Math.floor(Math.random() * 3);
+  let computerChoice;
+  switch (randomNo) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
       break;
     case 2:
-        computerChoice= "paper";
-        break;
-        case 3:
-            computerChoice = "scissors";
-            break;
+      computerChoice = "scissors";
+      break;
   }
-  console.log(`computer choose ${computerChoice}`);
   return computerChoice;
 };
+//!-------End here--------
 
-// ------------------------------------------
+// --------This is playRound Function---------
 
-const playRound =(userChoice,computerChoice)=>{
-    
-    if (userChoice === computerChoice) {
-        return 'The game is a tie';
-      }//Both select the Same choice
-    
-      if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-          return 'You Lost!';
-        } else {
-          return "You Won!"
-        }
-      } // end userchoice is rock
+function playRound(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "The game is a tie";
+  } //Both select the Same choice
 
-    
-      if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-          return 'You Lost!';
-        } else {
-          return 'You won!';
-        }
-      } // end userchoice is paper
-    
-    
-      if (userChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-          return 'You Lost!';
-        } else {
-          return 'You Won!';
-        }
-      } //end userchoice is scissors
-    
-};
-
-function game(){
-    let userInput=prompt("Enter your choice" );
-    let result=playRound(getUserChoice(userInput),getComputerChoice());
-    console.log(result);
-    return result;
-}
-
-let Pc=0;
-let human=0;
-let i=1;
-while(i<=5){
-   let result= game();
-    if("You Won!"===result){
-        human= human+1;
-        console.log(`------Human ${human}------Computer ${Pc}-------`);
-        i++;
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "You Lost!";
+    } else {
+      return "You Won!";
     }
-    else if("You Lost!"===result){
-        Pc=Pc+1;
-        console.log(`------Human ${human}------Computer ${Pc}-------`);
-        i++;
+  } // ?end userchoice is rock
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "You Lost!";
+    } else {
+      return "You Won!";
     }
+  } // ?end userchoice is paper
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "You Lost!";
+    } else {
+      return "You Won!";
+    }
+  } // ?end userchoice is scissors
 }
-(human>Pc) ? console.log(`Human ${human} Computer ${Pc}----human wins all the game`)
-:console.log(`Computer ${Pc} Human ${human}----Computer wins all the game`);
+//!-------End here--------
